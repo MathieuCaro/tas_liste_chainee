9736ec9bf2495fe00c2c8008016ebdd3bfc7d1e4
MathieuC98 <mathieucaro123@outlook.com>
2022-12-20 16:27:32 +0100

    ajout du fichier logger.c

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CC ?= gcc
 CFLAGS += -Wall  -g --pedantic -Werror -Wno-error=unused-but-set-variable
 
-LOCAL_LIB=/home/mathieucaro/C/allocation_memo-test/liste_chainee_alloc
-LOCAL_INC=/home/mathieucaro/C/allocation_memo-test/liste_chainee_alloc
+LOCAL_LIB=liste_chainee_alloc
+LOCAL_INC=liste_chainee_alloc
 
 all:
-	$(CC) $(LDFLAGS) -L$(LOCAL_LIB) -I$(LOCAL_INC) -o main $(CFLAGS) fonctions.c main.c -lm -llinkedlist 
+	$(CC) $(LDFLAGS) -L$(LOCAL_LIB) -I$(LOCAL_INC) -o main $(CFLAGS) fonctions.c logger.c main.c -lm -llinkedlist 
 
 test:
-	$(CC) $(LDFLAGS) -L$(LOCAL_LIB) -I$(LOCAL_INC) -o test_main $(CFLAGS) tests/test_main.c fonctions.c -lcunit -llinkedlist 
+	$(CC) $(LDFLAGS) -L$(LOCAL_LIB) -I$(LOCAL_INC) -o test_main $(CFLAGS) tests/test_main.c logger.c fonctions.c -lcunit -llinkedlist 
 
 lib:
 	make -C $(LOCAL_LIB)
--- fonctions.c
+++ fonctions.c
@@ -24,32 +24,6 @@ linked_list *first_fit(linked_list *head, int size)
     return NULL;
 }
 
-void write_log(char *action, int size, char *adresse)
-{
-    FILE *fichier;
-    fichier = fopen("fichier.txt", "a");
-
-    time_t timestamp = time(NULL);
-    struct tm *pTime = localtime(&timestamp);
-
-    char buffer[80];
-    strftime(buffer, 80, "[%B %d %Y][%R]", pTime);
-    fprintf(fichier, "%s %s %dbytes at adress %p\n", buffer, action, size, adresse);
-    fclose(fichier);
-}
-
-void read_log()
-{
-    FILE *fichier;
-    int taille;
-    char adress[20];
-    fichier = fopen("fichier.txt", "r");
-    while (fscanf(fichier, "%*s %*s %*s %*s %dbytes at adress %s\n",
-                  &taille, adress) == 2)
-        printf("%s %dbytes\n", adress, taille);
-    fclose(fichier);
-}
-
 char *tas_malloc(unsigned int taille)
 {
     linked_list *index = first_fit(espace_libre, taille);
